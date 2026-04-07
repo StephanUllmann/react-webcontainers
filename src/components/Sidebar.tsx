f36b4e32f3672dadae6f72045354a3f52787f5ea
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-04-07 18:45:17 +0200

    install html

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -66,7 +66,7 @@ export default function Sidebar({
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ bla: 'Jorge' }),
+          body: JSON.stringify({ hi: 'Jorge' }),
         }
       );
 
